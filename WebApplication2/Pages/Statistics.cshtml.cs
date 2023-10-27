1e419c46cac11289eb8c833ee021730e78a90808
hatim-scott <git config --global user.email mediamor@gmail.com>
2023-10-27 11:40:53 +0200

    Statistics

--- WebApplication2/Pages/Statistics.cshtml.cs
+++ WebApplication2/Pages/Statistics.cshtml.cs
@@ -18,17 +18,87 @@ namespace WebApplication2.Pages
 
         }
         
-        public double TotalOrders { get; set; }
-        public decimal TotalRevenue { get; set; }
+        public double AvgOrders { get; set; }
+        public double AvgAccountsRecivable { get; set; }
+        
+        
+        public class ClerkOrderCount
+        {
+            public string ClerkName { get; set; }
+            public int OrderCount { get; set; }
+        }
 
+        public List<ClerkOrderCount> ClerkOrderCounts { get; set; }
+        
+        public class DelOrderCount
+        {
+            public string DelName { get; set; }
+            public int OrderCount { get; set; }
+        }
 
+        public List<DelOrderCount> DelOrderCounts { get; set; }
+
+        public List<Order> OrderByDateToday { get; set; }
+        public List<Order> OrderByDateYesterday { get; set; }
+        public List<Order> OrderByDate2plus { get; set; }
+        
+        
         public async Task OnGetAsync()
         {
-            //Customers = await _context.Customers.ToListAsync();
-            TotalOrders = await _context.Orders.CountAsync(); // Exemple simulé
-          
-            TotalRevenue = 500.50m; // Exemple simulé
+
+            var orders = _context.Orders.Include(o => o.OrdersRows); 
+            double totalPrice = orders.Sum(order => order.OrdersRows.Sum(orderRow => orderRow.Quantity * orderRow.Product.Price));
+
+            
+            var orderqte = _context.Orders.Count();
+            AvgOrders = totalPrice/orderqte;
+
+            var cutqte = _context.Customers.Count();
+            
+            AvgAccountsRecivable = totalPrice/cutqte; 
+            
+            ClerkOrderCounts = _context.Clerks
+                .Select(clerk => new ClerkOrderCount
+                {
+                    ClerkName = clerk.Name,
+                    OrderCount = clerk.Orders.Count()
+                })
+                .ToList();
+            
+            DelOrderCounts = _context.Deliverers
+                .Select(del => new DelOrderCount
+                {
+                    DelName = del.Name,
+                    OrderCount = del.Orders.Count()
+                })
+                .ToList();
+
+            OrderByDateToday = _context.Orders
+                .Include(o=> o.Customer)
+                .Include(o=> o.Clerk)
+                .Include(o=> o.Deliverer)
+                .Where(o => o.OrderDate == "today")
+                .ToList();
+            
+            OrderByDateYesterday = _context.Orders
+                .Include(o=> o.Customer)
+                .Include(o=> o.Clerk)
+                .Include(o=> o.Deliverer)
+                .Where(o => o.OrderDate == "yesterday")
+                .ToList();
+            
+            OrderByDate2plus = _context.Orders
+                .Include(o=> o.Customer)
+                .Include(o=> o.Clerk)
+                .Include(o=> o.Deliverer)
+                .Where(o => o.OrderDate == "+2 days")
+                .ToList();
+
+
+
         }
+        
+        
         
     }
 }
